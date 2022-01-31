2325e2f1bc5b95f0176fb524ab7f161ccf9785db
monish karunakaran <ckmonish2000@gmail.com>
2022-01-31 17:33:56 +0530

    scaleable window

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import intro from "./intro.svg"
+import scene1 from "./scene1.json"
+import { useEffect, useState, useRef } from "react"
+import lottie from "lottie-web"
 
+
+const ratio = 1920 / 900
 function App() {
+  const [scale, setscale] = useState(window.innerWidth * 0.65 / 1000);
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    window.addEventListener("resize", onResize)
+
+    return () => {
+      window.removeEventListener("resize", onResize)
+    }
+  }, [])
+
+  const onResize = () => {
+    const scale = (window.innerWidth * 0.65 / 1000)
+    setscale(scale)
+  }
+
+  useEffect(() => {
+    if (containerRef.current) {
+      lottie.loadAnimation({
+        name: "placeholder",
+        container: containerRef.current,
+        renderer: "svg",
+        loop: true,
+        autoplay: true,
+        animationData: scene1,
+      })
+    }
+  }, [scene1, containerRef])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="vendorWrapper">
+      <div className="playHold" style={{
+        width: "1920px",
+        height: "900px",
+        transformOrigin: "210px 900px",
+        left: "-210px",
+        bottom: "0px",
+        transform: `scale(${scale})`
+      }}>
+
+        {/* background */}
+        <img src={intro} alt="" className="background" />
+        {/* btn */}
+        <img id="h" src="https://earlyedge.s3.ap-south-1.amazonaws.com/changes/11_evs_farm_and_jungle_y01/internal/images/efb860a4ccfb925473548e4162b6136a.svg" />
+      </div>
+
+
+      <div ref={containerRef}
+        style={{ width: "20%", position: "absolute", bottom: "0%" }}
+      >.</div>
+      {/* my imple */}
+
+      {/* <div className="parent">
+        <img src={intro} alt="" className="bg" />
+      </div>
+
+
+
+      {/* <h1 >hello</h1> */}
+
     </div>
   );
 }
